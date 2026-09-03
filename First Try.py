6a6a19238038f381fa584ac5ddbd77fe25fa19f4
Rotgarn <tech.m415@gmail.com>
2026-09-03 08:21:00 -0600

    Lección de str, int, float y boolean, más el como hacer inputs y mostrarlos con fstrings

--- First Try.py
+++ First Try.py
@@ -1,1 +1,7 @@
-print("Hello There!")
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+salario = float(input("Ingrese su salario deseado: "))
+salario = salario * 1.2  # Aumentamos el salario en un 20%
+
+print("Ayudanos confirmando tus datos:")
+print(f"Nombre: {nombre}, Edad: {edad}, Expectativa salarial: {salario}")
